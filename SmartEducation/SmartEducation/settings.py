03f6689c21da2b8e8c1a95dbacdfc8439d2356b6
Harrison Van Roy <harrison.vanroy007@gmail.com>
2020-08-25 18:48:17 +1000

    framework added and models for courses

--- SmartEducation/SmartEducation/settings.py
+++ SmartEducation/SmartEducation/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'WebScraper',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'SmartEducation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
--- SmartEducation/WebScraper/models.py
+++ SmartEducation/WebScraper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+
+class Assessment(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    weight = models.CharField(max_length=50)
+    course = models.ForeignKey(Course, related_name='assessment', on_delete=models.CASCADE)
