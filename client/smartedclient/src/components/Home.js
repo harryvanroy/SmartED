d5ab8edeb962d62e1e6771f1631989bf82f9660f
Harrison Van Roy <harrison.vanroy007@gmail.com>
2020-09-04 19:53:06 +1000

    grid

--- client/smartedclient/src/components/Home.js
+++ client/smartedclient/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 function Home() {
   return (
     <div>
-      Home page.
+      asdf
     </div>
   );
 };
