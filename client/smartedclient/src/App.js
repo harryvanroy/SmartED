076cfee1f3fcecbe4f1cfd5eff37fc461b13548a
Harrison Van Roy <harrison.vanroy007@gmail.com>
2020-09-04 16:41:36 +1000

    cleaned up navbar code

--- client/smartedclient/src/App.js
+++ client/smartedclient/src/App.js
@@ -1,9 +1,169 @@
 import React from 'react';
-import Demo from './demo';
 
-function App() {
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+
+import { makeStyles } from '@material-ui/core/styles';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import FeedbackIcon from '@material-ui/icons/Feedback';
+import SortIcon from '@material-ui/icons/Sort';
+import SchoolIcon from '@material-ui/icons/School';
+import ClassIcon from '@material-ui/icons/Class';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+import { Switch, Route, Link } from "react-router-dom";
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    background: '#51237a',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  nested: {
+    paddingLeft: theme.spacing(9),
+  },
+  listItemText:{
+    fontSize:'0.9em',
+  }
+}));
+
+  function App() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Demo />
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h4" noWrap style={{ flex: 1 }}>
+            SmartED
+          </Typography>
+          <AccountCircleIcon/>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button onClick={handleClick}>
+              <ListItemIcon><ClassIcon /></ListItemIcon>
+              <ListItemText primary="Courses" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                {['DECO3801', 'COMP3301', 'MATH3204', 'STAT2004'].map((text, index) => (
+                  <ListItem button className={classes.nested}>
+                    <ListItemText primary={text} classes={{primary:classes.listItemText}}/>
+                  </ListItem>
+                ))}
+              </List>
+            </Collapse>
+              {['Assessment', 'Course Goals', 'My Grades', 'Resources', 'Personal Feedback'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                  {
+                    { 
+                      0: <AssessmentIcon />,
+                      1: <EmojiEmotionsIcon />,
+                      2: <SchoolIcon />,
+                      3: <SortIcon />,
+                      4: <FeedbackIcon />
+                    }[index]
+                  }
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+{/*             <ListItem button>
+              <ListItemIcon><AssessmentIcon /></ListItemIcon>
+              <ListItemText primary="Assessment" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><EmojiEmotionsIcon /></ListItemIcon>
+              <ListItemText primary="Course Goals" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><SchoolIcon /></ListItemIcon>
+              <ListItemText primary="My Grades" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><SortIcon /></ListItemIcon>
+              <ListItemText primary="Resources" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><FeedbackIcon /></ListItemIcon>
+              <ListItemText primary="Personal Feedback" />
+            </ListItem> */}
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
   );
 };
 
