36972d9cb32700babcb0db254d72e176e73def4a
Harrison Van Roy <harrison.vanroy007@gmail.com>
2020-09-04 16:41:36 +1000

    cleaned up navbar code

---
+++ client/smartedclient/src/components/Assessment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Assessment() {
+  return (
+    <div>
+      Assessment page.
+    </div>
+  );
+};
+
+export default Assessment;
---
+++ client/smartedclient/src/components/Grades.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Grades() {
+   return (
+     <div>
+       Grades page.
+     </div>
+   );
+};
+
+export default Grades;
