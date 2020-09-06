47b8e024a6fd59b2a000d58156c5663e745d4c28
George Mulhearn <immpumpz@gmail.com>
2020-09-06 15:39:33 +1000

    moved database app

--- server/smarteducation/Database/models.py
+++ server/smarteducation/Database/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 from django.db import models
 
+
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
+
 
 class Resource(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,6 +20,7 @@ class Resource(models.Model):
     dateAdded = models.DateTimeField('date published')
     week = models.IntegerField()
 
+
 class File(models.Model):
     id = models.AutoField(primary_key=True)
     path = models.FileField(upload_to='uploads/')
@@ -25,15 +28,13 @@ class File(models.Model):
     size = models.IntegerField()
     course = models.ForeignKey(Resource, on_delete=models.SET_NULL, blank=True, null=True)
 
+
 class Institution(models.Model):
-	name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
 
 class Course(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     semester = models.CharField(max_length=255, null=True)
     institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, blank=True, null=True)
-
-
-
-
