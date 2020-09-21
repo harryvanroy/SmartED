81ebaff0809e9b5b8e4066bc5456d03c59504862
Will Jago <48144925+wjago@users.noreply.github.com>
2020-09-21 18:30:00 +1000

    Update Goals.js

--- smarteducation/reactapp/src/components/Goals.js
+++ smarteducation/reactapp/src/components/Goals.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Button, Container, Box, Typography } from '@material-ui/core';
 
 function Goals() {
   return (
-    <div>
-      Goals page.
-    </div>
+    <Box width="80%">
+      <Typography variant="h4">
+        Course goals
+      </Typography>
+      
+      <Button variant="contained" color="primary" size="large" m={10}>
+        ADD/CHANGE
+      </Button>
+    </Box>
   );
 };
 
