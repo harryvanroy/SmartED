bef86c4ce4c0d5f8a998b987401b65bd6dc38571
George Mulhearn <immpumpz@gmail.com>
2020-09-21 14:28:01 +1000

    migrated react into django successfully

---
+++ server/smarteducation/smartedclient/src/components/Goals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Goals() {
+  return (
+    <div>
+      Goals page.
+    </div>
+  );
+};
+
+export default Goals;
---
+++ server/smarteducation/smartedclient/src/components/Resources.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Resources() {
+  return (
+    <div>
+      Resources page.
+    </div>
+  );
+};
+
+export default Resources;
