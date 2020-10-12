b1f40c73d30bc0463de4b640bc5e97c1649ee5ba
George Mulhearn <immpumpz@gmail.com>
2020-10-12 10:55:20 +1000

    db works

--- smarted/Database/models.py
+++ smarted/Database/models.py
@@ -69,7 +69,7 @@ class File(models.Model):
 
 
 class Institution(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return f"{self.name}"
