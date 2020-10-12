5749af47c4a5503dcbe2574028d2d05853a7ca2d
TripleExclam <burtonmatthew00@gmail.com>
2020-10-12 11:23:48 +1000

    Added admin interface for database

--- smarted/Database/admin.py
+++ smarted/Database/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Database.models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(StudentCourse)
+admin.site.register(Institution)
